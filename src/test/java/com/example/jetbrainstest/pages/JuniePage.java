603276c4861cb4dff9b8f74bc058f92d4f4febbe
Darya <Darksoul452@mail.ru>
2025-10-24 20:39:34 +0300

    Correction JuniePage + JuniePageTest created for lesson 5.3.

--- src/test/java/com/example/jetbrainstest/pages/JuniePage.java
+++ src/test/java/com/example/jetbrainstest/pages/JuniePage.java
@@ -6,30 +6,69 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class JuniePage {
     private final AllureLogger LOG = new AllureLogger(LoggerFactory.getLogger(JuniePage.class));
     WebDriver driver;
 
     @FindBy(xpath = "//a[contains(@href, '26104-jetbrains-junie-eap')]")
-    public static WebElement junieDownloadButton;
+    public WebElement junieDownloadButton;
 
     @FindBy(xpath = "//button[contains(@class, 'test_download_plugin_button')]")
-    public static WebElement installJunieButton;
+    public WebElement getJunieButton;
+
+    @FindBy(xpath = "//button[contains(text(), 'Accept') or contains(text(), 'Принять')]")
+    private WebElement acceptCookiesButton;
+
+    @FindBy(xpath = "//h2[text()='Plugin Versions']")
+    private WebElement pluginVersionsTable;
+
+    @FindBy(xpath = "//a[contains(@href, 'junie-github')]")
+    private WebElement junieGit;
+
+    @FindBy(xpath = "//a[contains(@href, 'join-waitlist')]")
+    private WebElement signUp;
 
     public void junieDownloadButton() {
-        LOG.info("Проверка кликабельности кнопки Download");
+        LOG.info("Нажатие кнопки Download");
         junieDownloadButton.click();
     }
 
-    public void installJunieButton() {
-        LOG.info("Проверка кликабельности кнопки Install to IDE");
-        installJunieButton.click();
+    public void getJunieButton() {
+        LOG.info("Нажатие кнопки GET");
+        getJunieButton.click();
     }
 
     public JuniePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
+    public void acceptCookiesOnNewPage() {
+        LOG.info("Проверка модального окна с куки");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.elementToBeClickable(acceptCookiesButton));
+        acceptCookiesButton.click();
+    }
+
+    public boolean isPluginVersionsTableDisplayed() {
+        LOG.info("Проверка отображения таблицы версий плагина");
+        return pluginVersionsTable.isDisplayed();
+    }
+
+    public void junieGit() {
+        LOG.info("Нажатие кнопки Junie in GitHub");
+        junieGit.click();
+    }
+
+    public boolean signUp() {
+        LOG.info("Проверка переключения на GitHub");
+        return signUp.isDisplayed();
+    }
 }
+
--- src/test/java/com/example/jetbrainstest/tests/JuniePageTest.java
+++ src/test/java/com/example/jetbrainstest/tests/JuniePageTest.java
@@ -5,32 +5,46 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class JuniePageTest extends BaseTest {
-    private JuniePage JuniePage;
+    private JuniePage juniePage;
 
     @BeforeEach
     @Override
     public void setUp() {
         super.setUp();
         getDriver().get("https://www.jetbrains.com/junie/#");
-        JuniePage juniePage = new JuniePage(getDriver());
+        juniePage = new JuniePage(getDriver());
     }
 
     @Test
-    @DisplayName("Проверка нажатия кнопки Download")
+    @DisplayName("Нажатие кнопки Download и переход на страницу скачивания")
     public void junieDownloadButton() {
-        JuniePage.junieDownloadButton();
-        assertEquals("https://www.jetbrains.com/junie/#", getDriver().getCurrentUrl(), "не верная ссылка");
+        juniePage.junieDownloadButton();
+        ArrayList<String> tabs = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(tabs.get(1));
+
+        juniePage.acceptCookiesOnNewPage();
+
+        assertTrue(getDriver().getCurrentUrl().contains("26104-junie-the-ai-coding-agent-by-jetbrains"),
+                "Ссылка не содержит ожидаемую часть");
+
+        juniePage.getJunieButton();
+        assertTrue(juniePage.isPluginVersionsTableDisplayed(),
+                "Таблица с версиями плагина не отобразилась после нажатия кнопки GET");
     }
 
     @Test
-    @DisplayName("Проверка нажатия кнопки 'загрузка'")
-    public void installJunieButton() {
-        JuniePage.installJunieButton();
-        assertEquals("https://plugins.jetbrains.com/plugin/26104-jetbrains-junie", getDriver().getCurrentUrl(), "не верная ссылка");
+    @DisplayName("Нажатие кнопки Juine in Github")
+    public void JunieGit() {
+        juniePage.junieGit();
+        juniePage.acceptCookiesOnNewPage();
+        assertTrue(juniePage.signUp(),
+                "переключение на junie-github не произошло");
     }
 }
 
