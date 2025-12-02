305432200ad38fb79586e0fbfd8ec130b24c925c
Yudessa <Darksoul452@mail.ru>
2025-12-02 11:11:20 +0300

    added ExtendWith

--- src/test/java/com/example/jetbrainstest/tests/JuniePageTest.java
+++ src/test/java/com/example/jetbrainstest/tests/JuniePageTest.java
@@ -1,13 +1,15 @@
 package com.example.jetbrainstest.tests;
 
+import com.example.jetbrainstest.MyExtension;
 import com.example.jetbrainstest.pages.JuniePage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@ExtendWith(MyExtension.class)
 public class JuniePageTest extends BaseTest {
     private JuniePage juniePage;
 
